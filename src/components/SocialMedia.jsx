7c8319e7fd333b7c487060f0f199cd9519c7140d
malik <malikagboola3@gmail.com>
2022-11-21 00:43:25 +0100

    new

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BsTwitter, BsGithub } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter href="https://twitter.com/Manlikemalikk" />
+    </div>
+    <div>
+      <BsGithub href="https://github.com/Manlikemaleek" />
+    </div>
+  </div>
+
+);
+
+export default SocialMedia;
